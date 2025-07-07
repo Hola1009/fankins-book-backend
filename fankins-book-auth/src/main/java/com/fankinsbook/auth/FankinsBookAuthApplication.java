d89fffca738354f5bc80e764c696b20f5e13ae81
hola1009 <fancier_fox@163.com>
2025-07-07 19:39:53 +0800

    项目初始化

---
+++ fankins-book-auth/src/main/java/com/fankinsbook/auth/FankinsBookAuthApplication.java
@@ -0,0 +1,13 @@
+package com.fankinsbook.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FankinsBookAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FankinsBookAuthApplication.class, args);
+    }
+
+}
---
+++ fankins-book-auth/src/test/java/com/fankinsbook/auth/FankinsBookAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fankinsbook.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FankinsBookAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
